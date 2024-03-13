b4756d2474f473216887f07134f0cf7d01c41d34
tkostadinov004 <tkostadinov04@gmail.com>
2024-03-13 20:47:59 +0200

    Fix

--- Practicums/Pract 4/Task 1 - write numbers to binary file.cpp
+++ Practicums/Pract 4/Task 1 - write numbers to binary file.cpp
@@ -3,48 +3,45 @@
 
 int* getNumbers(size_t count)
 {
-    int* arr = new int[count];
-    for (size_t i = 0; i < count; i++)
-    {
-        std::cin >> arr[i];
-    }
-    return arr;
+	int* arr = new int[count];
+	for (size_t i = 0; i < count; i++)
+	{
+		std::cin >> arr[i];
+	}
+	return arr;
 }
 void writeToFile(std::ofstream& ofs, const int* arr, size_t size)
 {
-    if (!arr)
-    {
-        return;
-    }
+	if (!arr)
+	{
+		return;
+	}
 
-    ofs.write((const char*)&size, sizeof(size));
-    for (size_t i = 0; i < size; i++)
-    {
-        ofs.write((const char*)&arr[i], sizeof(arr[i]));
-    }
+	ofs.write((const char*)&size, sizeof(size));
+	ofs.write((const char*)arr, sizeof(int) * size);
 }
 void writeToFile(const char* filename)
 {
-    if (!filename)
-    {
-        return;
-    }
+	if (!filename)
+	{
+		return;
+	}
 
-    size_t count;
-    std::cin >> count;
+	size_t count;
+	std::cin >> count;
 
-    int* arr = getNumbers(count);
+	int* arr = getNumbers(count);
 
-    std::ofstream ofs(filename, std::ios::binary);
-    if (!ofs.is_open())
-    {
-        return;
-    }
-    writeToFile(ofs, arr, count);
+	std::ofstream ofs(filename, std::ios::binary);
+	if (!ofs.is_open())
+	{
+		return;
+	}
+	writeToFile(ofs, arr, count);
 
-    delete[] arr;
+	delete[] arr;
 }
 int main()
 {
-    writeToFile("nums.dat");
+	writeToFile("nums.dat");
 }
